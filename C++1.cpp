df60e50ab83c71ffdc854cedfbe8d11e7a8c2f5c
paparcrane <58927692+paparcrane@users.noreply.github.com>
2020-04-17 14:45:37 +0800

    创建test.cpp文件
    第一次使用github创建项目文件

---
+++ C++1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include <string>
+
+class Person
+{
+public:
+
+	Person(string name)
+	{
+		this->m_Name = name;
+	}
+
+	void sayHello(Person &p)
+	{
+		cout << p.m_Name << " : ";
+		cout << "Hello World" << endl;
+	}
+
+	string m_Name;
+};
+
+void test01()
+{
+	Person p("zhangsan");
+	p.sayHello(p);
+}
+
+int main()
+{
+	test01();
+
+	system("pause");
+	return 0;
+}
